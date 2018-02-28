5b22d483e5ad83e176090afd0d3dccae9ad7f568
kaleb mcghie <zarlo5899@gmail.com>
2018-02-28 11:12:06 +1100

    name fixs

--- hdf5helper.py
+++ hdf5helper.py
@@ -11,14 +11,14 @@ class FileHelper(object):
     def get(self):
         return self.db
 
-    def get(self, Path):
-        return self.db[Path]
+    def get(self, path):
+        return self.db[path]
 
     def list(self, path):
         return [key for key in self.db[path].keys()]
 
-    def store_file(self, Path, store_path):
-        file = open(Path, 'rb')
+    def store_file(self, path, store_path):
+        file = open(path, 'rb')
         try:
             dt = h5py.special_dtype(vlen=np.dtype('uint8'))
             temp = self.db.create_dataset(store_path, (1,), dtype=dt)
@@ -28,7 +28,6 @@ class FileHelper(object):
         temp[0] = np.fromstring(file.read(), dtype='uint8')
 
     def store_from_folder(self, path, save_path=""):
-
         directory = os.fsencode(path)
         for file in os.listdir(directory):
             filename = os.fsdecode(file)
